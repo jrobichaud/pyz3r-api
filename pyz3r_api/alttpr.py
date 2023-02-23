7520c2f93ccf44495bb5ced7ec9def773e9966ba
Jules Robichaud-Gagnon <j.robichaudg@gmail.com>
2023-02-23 16:39:01 -0500

    generate

--- pyz3r_api/alttpr.py
+++ pyz3r_api/alttpr.py
@@ -1,4 +1,3 @@
-from pyz3r.customizer import convert2settings
 import datetime
 
 import pyz3r
@@ -22,6 +21,22 @@ async def daily():
     daily_hash = await alttpr.find_daily_hash()
     name = f"alttpr Daily {datetime.datetime.now(tz=datetime.timezone.utc).date().isoformat()} {daily_hash}.sfc"
     seed = await alttpr.retrieve(daily_hash)
+    game = await seed.create_patched_game(
+        current_app.config["ROM_PATH"], spritename=sprite
+    )
+    response = make_response(game.rom)
+    response.headers.set("Content-Type", "application/octet-stream")
+    response.headers.set("Content-Disposition", "attachment", filename=name)
+    return response
+
+
+@bp.post("/generate")
+async def generate():
+    alttpr = pyz3r.alttpr.ALTTPR()
+    sprite = request.json.get("sprite", "Link")
+    settings = request.json.get("settings")
+    seed = await alttpr.generate(settings=settings)
+    name = f'alttpr {datetime.datetime.now(tz=datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")} {seed.hash}.sfc'
     game = await seed.create_patched_game(
         current_app.config["ROM_PATH"], spritename=sprite
     )
--- tests/test_alttpr.py
+++ tests/test_alttpr.py
@@ -15,11 +15,41 @@ def test_config():
 
 @pytest.mark.asyncio
 def test_daily(client):
-    response = client.post("/alttpr/daily", json={"sprite": "Peach"})
+    response = client.post("/alttpr/daily", json={"sprite": "Princess Peach"})
     assert type(response.data) == bytes
 
 
 @pytest.mark.asyncio
 def test_settings(client):
     response = client.get("/alttpr/settings")
-    assert type(response.data) == str
+    assert type(response.json) == dict
+
+
+@pytest.mark.asyncio
+def test_generate(client):
+    response = client.post(
+        "/alttpr/generate",
+        json={
+            "sprite": "Princess Peach",
+            "settings": {
+                "accessibility": "items",
+                "boss_shuffle": "none",
+                "dungeon_items": "standard",
+                "enemy_damage": "default",
+                "enemy_health": "default",
+                "enemy_shuffle": "none",
+                "entrance_shuffle": "none",
+                "ganon_open": "7",
+                "glitches_required": "none",
+                "goal": "ganon",
+                "hints": "on",
+                "item_functionality": "normal",
+                "item_placement": "advanced",
+                "item_pool": "normal",
+                "tower_open": "7",
+                "weapons": "randomized",
+                "world_state": "open",
+            },
+        },
+    )
+    assert type(response.data) == bytes
